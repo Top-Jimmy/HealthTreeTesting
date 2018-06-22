4d4d3b445ac08ec8943674a8ad3d5d476f2ed4fc
etidd2 <elliottidd1@gmail.com>
2018-06-22 10:02:23 -0600

    Removed files from git

--- main.py
+++
@@ -1,8 +0,0 @@
-# base_url = 'https://www.healthtree.org/'
-# env = 'prod'
-# base_url = 'https://healthtreeapp.herokuapp.com/'
-# env = 'test'
-base_url = 'http://localhost:3001/'
-env = 'local'
-
-browser = 'chrome'
