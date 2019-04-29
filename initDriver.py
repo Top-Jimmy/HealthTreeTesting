efe9c7db501869a17adf288b12ddb3af55ea0b57
Top-Jimmy <tiddandrew@gmail.com>
2019-04-29 14:50:32 -0600

    Chromedriver

--- initDriver.py
+++ initDriver.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 import main
 
@@ -12,13 +13,26 @@ def get_desired_caps(browser):
     desired_caps = webdriver.DesiredCapabilities.CHROME
   return desired_caps
 
+def get_chrome_options():
+  #path = "/Users/andrewtidd/Library/Application Support/Google/Chrome/Default"
+  options = Options()
+  #options.add_argument("user-data-dir=" + path)
+  if main.headless:
+    options.add_argument("--headless")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+  return options
+
 def start(browser=None):
   driver_url = 'http://127.0.0.1:5050/wd/hub'
   if browser is None:
     browser = 'chrome'
 
   desired_caps = get_desired_caps(browser)
-  driver = webdriver.Remote(driver_url, desired_caps)
+  if browser == 'chrome':
+    driver = webdriver.Chrome(chrome_options=get_chrome_options())
+  else:
+    driver = webdriver.Remote(driver_url, desired_caps)
 
   # set default window settings
   driver.set_window_size(1350, 950)
--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 # env = 'prod'
 base_url = 'https://healthtree-app-staging.herokuapp.com/signin'
 env = 'test'
+headless = False
 # base_url = 'http://localhost:3001/'
 # env = 'local'
 
 browser = 'chrome'
+
